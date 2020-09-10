970a5451513b4f0fe1bbd09ed830fa5f4c78509b
Tinu George <tinu.george@outlook.com>
2020-09-10 14:48:22 +0800

    add react-fontawesome lib

--- components/header.js
+++ components/header.js
@@ -1,33 +1,12 @@
 import Container from "./container";
-import TopBar from "./topbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
 
 const Header = ({ siteTitle }) => {
     return (
         <>
-            <TopBar>
-                <Container>
-                    <div className="newsletter">fi</div>
-                    <div className="social-icons">dd</div>
-                </Container>
-            </TopBar>
-            <header>
-                <Container>
-                    <div className="site-title">{siteTitle}</div>
-                </Container>
-            </header>
-            <style jsx>{`
-                .newsletter {
-                    display: flex;
-                    align-self: flex-start;
-                    flex: 4 1 auto;
-                }
-                .social-icons {
-                    display: flex;
-                    flex-direction: row-reverse;
-                    align-self: flex-end;
-                    flex: 1 1 auto;
-                }
-            `}</style>
+            <header></header>
+            <style jsx>{``}</style>
         </>
     );
 };
